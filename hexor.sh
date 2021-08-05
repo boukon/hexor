f781cff208545a8387a4e9b3736162ea36127306
亡魂 <88466197+boukon@users.noreply.github.com>
2021-08-05 18:30:52 +0000

    Update hexor.sh

--- hexor.sh
+++ hexor.sh
@@ -2,16 +2,26 @@
 #
 # printfは必要なプログラムです。
 #
-# 古い用法、
+# 用法、
 # $ hexor aaA0fa 00bf34 4
 # aa1f00ce$
-#
-# 今の用法、
-# 「$ hexor aa 0f ee」と同義語 「$ hexor $(hexor aa 0f) ee」です。
-# d_mojiにフラグは「-n 3」です。
-a=$1; b=$2
-[ "$3" ] && d_moji=$3 || d_moji=2
+
+# Ensure `a' is longer than `b'
+[[ ${#1} -ge ${#2} ]] && { a=$1; b=$2; } || { b=$1; a=$2; }
+
+[ "$3" ] && d_moji=$3 || {
+ # default to 2 hex digits to 1 byte; this is Bash's own behavior.
+ # Bash automatically prepends, if necessary, a zero to the last or only byte
+ printf %0${#a}x $((0x$a ^ 0x$b)) 
+ exit
+}
+
+# Ensure no syntax errors e.g. $((0x ^ 0x12))
 a_b=$((${#a}-${#b}))
-[ $a_b -gt 0 ] && printf -v b $b%0${a_b}d   || {
-[ $a_b -lt 0 ] && printf -v a $a%0$((0-a_b))d; }
-printf %0${#a}x $((0x$a ^ 0x$b))
+[ $a_b -gt 0 ] && printf -v b $b%0$((${#a}-${#b}))d
+
+for ((i=0; i < ${#a}; i+=d_moji)) { # odd-byte
+ printf %0${d_moji}x $((
+  0x${a:$i:$d_moji} ^ 0x${b:$i:$d_moji}
+ ))
+}
