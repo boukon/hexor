2d95bc66934fb65b69011bfb6ee6893c73fcb881
亡魂 <88466197+boukon@users.noreply.github.com>
2021-08-05 17:44:05 +0000

    Update hexor.sh

--- hexor.sh
+++ hexor.sh
@@ -2,17 +2,16 @@
 #
 # printfは必要なプログラムです。
 #
-# 用法、
+# 古い用法、
 # $ hexor aaA0fa 00bf34 4
 # aa1f00ce$
 #
+# 今の用法、
+# 「$ hexor aa 0f ee」と同義語 「$ hexor $(hexor aa 0f) ee」です。
+# d_mojiにフラグは「-n 3」です。
 a=$1; b=$2
 [ "$3" ] && d_moji=$3 || d_moji=2
 a_b=$((${#a}-${#b}))
 [ $a_b -gt 0 ] && printf -v b $b%0${a_b}d   || {
 [ $a_b -lt 0 ] && printf -v a $a%0$((0-a_b))d; }
-for ((i=0; i < ${#a}; i+=d_moji)) {
- printf %0${d_moji}x $((
-  0x${a:$i:$d_moji} ^ 0x${b:$i:$d_moji}
- ))
-}
+printf %0${#a}x $((0x$a ^ 0x$b))
